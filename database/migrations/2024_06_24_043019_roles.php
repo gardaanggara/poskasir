53e8f099a1e2e1e224cd38c62853fd9024cc963a
gardaanggara <shijhiho@gmail.com>
2024-07-17 16:09:47 +0700

    fix: insert created and updated

--- database/migrations/2024_06_24_043019_roles.php
+++ database/migrations/2024_06_24_043019_roles.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('roles', function (Blueprint $table) {
             $table->id();
             $table->string('keterangan');
-            $table->timestamp('created_at');
+            $table->timestamps();
         });
     }
 
